dc81bae2a2d401f3aac524c6fe49d306a87474dc
Koos85 <koosjoosten@gmail.com>
2025-11-18 16:57:57 +0100

    work on checks

--- lib/query.py
+++ lib/query.py
@@ -1,21 +1,41 @@
+from collections import defaultdict
 from libprobe.asset import Asset
 from libprobe.exceptions import CheckException
-from typing import List, Dict
+from pypureclient import flasharray
+from pypureclient.exceptions import PureError
+from .version import __version__
 
 
-async def query(
+USER_AGENT = f'InfraSonarPureStorageProbe/{__version__}'
+
+CONN_CACHE: dict[tuple[int, str, str], flasharray.Client] = \
+    defaultdict(flasharray.Client)
+
+
+async def get_client(
         asset: Asset,
         local_config: dict,
-        config: dict,
-        route: str) -> List[Dict]:
+        config: dict) -> flasharray.Client:
 
     address = config.get('address')
     if not address:
         address = asset.name
-    username = local_config.get('username')
-    password = local_config.get('password')
-    if None in (username, password):
+    token = local_config.get('secret')
+    if token is None:
         raise CheckException('missing credentials')
 
-    # TODO
-    return []
+    conn = CONN_CACHE.get((asset.id, address, token))
+    if conn:
+        return conn
+
+    try:
+        conn = flasharray.Client(
+            target=address,
+            api_token=token,
+            user_agent=USER_AGENT,
+        )
+    except PureError:
+        raise ConnectionError('Unable to connect')
+    else:
+        CONN_CACHE[(asset.id, address, token)] = conn
+    return conn
