8bcc7db21af53934502b687081363fc4400de9d6
Jeroen van der Heijden <jeroen@cesbit.com>
2025-11-19 12:41:01 +0100

    pep

--- lib/query.py
+++ lib/query.py
@@ -82,7 +82,7 @@ async def query(
     )
     if isinstance(result, flasharray.ErrorResponse):
         status_code = result.status_code  # type: ignore
-        logging.error( f'Invalid response status {status_code} ({result})')
+        logging.error(f'Invalid response status {status_code} ({result})')
         messages = ', '.join(err.message for err in result.errors)
         raise CheckException(
             f'Invalid response status {status_code} ({messages})')
